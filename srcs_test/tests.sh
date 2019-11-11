5496f9777064b391c31dff1a5b16c6fc33086a4f
Maofryy <maori.benhassine@gmail.com>
2019-11-11 16:42:19 +0100

    push

--- srcs_test/tests.sh
+++ srcs_test/tests.sh
@@ -15,22 +15,25 @@ echo "$1 tests of $2 ints between $3 and $4"
 while [ $NB -ne 0 ]
 do
   TEST=$(perl srcs_test/gen_int_lst.prl $2 $3 $4)
-  ./push_swap.exe $TEST > ret_ps
-  cat ret_ps | ./checker.exe $TEST > ret_file
+  ./push_swap.exe $TEST > ps.ret
+  cat ps.ret | ./checker.exe $TEST > file.ret
   # ./push_swap.exe $TEST | ./checker.exe $TEST > ret_file
-  KO_RET=$(cat ret_file | grep KO | wc -l)
-  NB_OP=$(cat ret_file | grep ops. |cut -d ' ' -f 1)
+  KO_RET=$(cat file.ret | grep KO | wc -l)
+  NB_OP=$(cat file.ret | grep ops. |cut -d ' ' -f 1)
 
   if [ $NB_OP -gt $MAX ]
   then
+    cat ps.ret > max_ps.ret
+    cat file.ret > max_file.ret
+    echo $TEST > max_test.ret
     MAX=$NB_OP
   fi
 
 
   # echo $NB
-  echo "$TEST : "
-  paste -d ' ' -s ret_ps ret_file
-  echo ' '
+  # echo "$TEST : "
+  # paste -d ' ' -s ps.ret file.ret
+  # echo ' '
 
   if [ $KO_RET -ge 1 ]
   then
@@ -43,4 +46,5 @@ do
   NB=$[$NB-1];
 done
 
-echo "Max number of operations :" $MAX
+echo "Max number of operations $MAX :"
+cat max_ps.ret max_file.ret max_test.ret
